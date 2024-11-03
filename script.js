7c8f39a4449a4ad115c089344b4fce91cba1a634
mrubaish <mrubaish16@gmail.com>
2024-11-03 14:28:16 +0500

    some-code

---
+++ script.js
@@ -0,0 +1,13 @@
+let a = 0
+alert(123 + 123)
+let rub = 0
+alert(123 / 123)
+
+let rubaish = 0
+alert(123 - 123)
+let khizra = 0
+alert(123 * 123)
+let fgj = 0
+alert((123))
+let asd = 0
+alert(123 % 123)
